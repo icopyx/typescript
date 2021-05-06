cf0f7e32af2d08199e5cf89256e22d6b98375a30
hsyou <hsyou@incruit.com>
2021-05-06 21:12:47 +0900

    #0.5 완료

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string): string => {
+   return `hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("Hongsun22",42,"male"));
+
+export {};
